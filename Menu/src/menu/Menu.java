297930c07d862d9387c97016113948cf39a98e42
Jossie Castrillo <bkcastrillo@gmail.com>
2017-02-26 18:29:34 -0600

    interfaz grafica
    creacion del menu

---
+++ Menu/src/menu/Menu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu;
+
+/**
+ *
+ * @author jossie
+ */
+public class Menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        inicio_menu ventana1=new inicio_menu();
+        ventana1.setVisible(true);
+        
+      
+    }
+    
+}
